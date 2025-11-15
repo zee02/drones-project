142c05d0bec04117dcb8e91d8a58ff4d0266e997
José Fernandes <zefrancisco2002@gmail.com>
2025-11-15 18:55:32 +0000

    feat: vendor dix

--- storage/framework/views/2ca7a331e39ac0037900004388d91216.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php $__env->startSection('title', __('Unauthorized')); ?>
-<?php $__env->startSection('code', '401'); ?>
-<?php $__env->startSection('message', __('Unauthorized')); ?>
-
-<?php echo $__env->make('errors::minimal', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH B:\Projetos\website\drones-project\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions\views\401.blade.php ENDPATH**/ ?>
