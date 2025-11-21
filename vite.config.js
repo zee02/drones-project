281a973d11d8b1cfb8fabaca51b2f5930c0c4e32
José Fernandes <zefrancisco2002@gmail.com>
2025-11-21 15:26:24 +0000

    feat: deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,14 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    base: '/',
-    
-    server: {
-        hmr: process.env.LARAVEL_APP_ENV === 'production' ? false : {
-            host: 'localhost',
-            port: 5173,
-        },
-    },
 });
