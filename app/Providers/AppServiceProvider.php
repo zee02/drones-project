36a7c8b8813314038c167b99fc5549076b051a9d
José Fernandes <zefrancisco2002@gmail.com>
2025-11-21 14:10:24 +0000

    feat: deploy config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route; 
-
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
 
     public function boot(): void
     {
-        
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
