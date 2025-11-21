8343759761810776d2f8e92553ecddf4668d50d5
José Fernandes <zefrancisco2002@gmail.com>
2025-11-21 14:25:01 +0000

    feat: deploy config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,12 +8,12 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
 
